e8795899e456ac0a8abec2671ea5bd240a1454ea
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-09 21:30:32 +0530

    Coupon number problem

---
+++ CouponNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    public class CouponNumber
+    {
+        public int N;
+        public static void Coupon(int N)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@
             //Fibonacci.PrintingFibonacci();
             //PerfectNumber.ThePerfectNumber();
             //PrimeNumber.FindThePrimeNumber();
-            ReverseNumber.ReverseTheNumber();
+            //ReverseNumber.ReverseTheNumber();
+            Console.WriteLine("Enter N Value");
+            CouponNumber c = new CouponNumber();
+            int N = Convert.ToInt32(Console.ReadLine());
+            CouponNumber.Coupon(N);
         }
     }
 }
